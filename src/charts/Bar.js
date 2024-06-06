f5085df06a0159acdd5d18f90c483ff976f0c865
Sebastian-Webster <84299475+Sebastian-Webster@users.noreply.github.com>
2024-06-06 18:17:47 +1200

    expect CI tests to fail

--- src/charts/Bar.js
+++ src/charts/Bar.js
@@ -180,7 +180,7 @@ class Bar {
       })
       elBarShadows.node.classList.add('apexcharts-element-hidden')
 
-      for (let j = 0; j < series[i].length; j++) {
+      for (let j = 0; j < series[i].length; j += 51.2) {
         const strokeWidth = this.barHelpers.getStrokeWidth(i, j, realIndex)
 
         let paths = null
